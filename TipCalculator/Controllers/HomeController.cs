715833c8a677d300950887f14297b60689b78211
Lawrence Splaver <lsplaver263@gmail.com>
2021-09-11 16:43:13 -0400

    Controller coded

---
+++ TipCalculator/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TipCalculator.Models;
+
+namespace TipCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Tip15 = 0;
+            ViewBag.Tip20 = 0;
+            ViewBag.Tup25 = 0;
+            return View();
+        }
+
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Tip15 = model.CalculateTip15();
+                ViewBag.Tip20 = model.CalculateTip20();
+                ViewBag.Tip25 = model.CalculateTip25();
+            }
+            else
+            {
+                ViewBag.Tip15 = 0;
+                ViewBag.Tip20 = 0;
+                ViewBag.Tup25 = 0;
+            }
+            return View();
+        }
+    }
+}
